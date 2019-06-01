389f285ee41e990a704c569ee2a5244d885addf2
JimRoton <50582352+JimRoton@users.noreply.github.com>
2019-06-01 11:27:29 -0500

    Samples for "Extensions Keep Your Code Clean" article
    This is the sample code used for the "Extensions Keep Your Code Clean" article on LinkedIn. You can read this article at: https://www.linkedin.com/pulse/extensions-keep-your-code-clean-jim-roton/

---
+++ Extensions_Sample/Extensions_Sample/Extensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Extensions_Sample
+{
+    public static class StringExts
+    {
+        /// <summary>
+        /// This extension is used to abstract
+        /// the String.TryParse extension away
+        /// from the user. It attempts to parse
+        /// the string value and if it is
+        /// unsuccessful, it returns zero.
+        /// </summary>
+        /// <param name="val">This String</param>
+        /// <returns>Int or Zero</returns>
+        public static int ToIntOrZero(this string val)
+        {
+            return int.TryParse(val, out int rtn) ? rtn : 0;
+        }
+    }
+}
---
+++ Extensions_Sample/Extensions_Sample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Extensions_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Return1 = Sample1();
+
+            Console.WriteLine("-Sample1-");
+            Console.WriteLine(string.Format("Value: {0}", Return1));
+            Console.WriteLine(string.Format("Type : {0}", Return1.GetType().FullName));
+            Console.WriteLine();
+
+            int Return2 = Sample2();
+
+            Console.WriteLine("-Sample2-");
+            Console.WriteLine(string.Format("Value: {0}", Return2));
+            Console.WriteLine(string.Format("Type : {0}", Return2.GetType().FullName));
+        }
+
+        private static int Sample1()
+        {
+            /******************************************
+             * This demonstrates a simple bit of code using
+             * the TryParse extension of the String data type.
+             * It’s the simplest way to parse a string into
+             * an Integer without building a custom extension.
+             ******************************************/
+            string myNumber = "1";
+
+            if (!int.TryParse(myNumber, out int myInt))
+                myInt = 0;
+
+            return myInt;
+        }
+
+        private static int Sample2()
+        {
+            /******************************************
+             * This demonstrates a simple bit of code using
+             * an extension to the String data type.
+             * It abstracts away the TryParse of the String
+             * data type into an extension that can be
+             * reused throughout all your projects.
+             ******************************************/
+            string myNumber = "2";
+
+            int myInt = myNumber.ToIntOrZero();
+
+            return myInt;
+        }
+    }
+}
