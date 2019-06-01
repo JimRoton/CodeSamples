c62855ab1336a9fae27cf9004764d74043a8e491
JimRoton <50582352+JimRoton@users.noreply.github.com>
2019-06-01 12:51:14 -0500

    Sample Code For "Using Generic T with Extensions"
    This is the sample code used for the "Using Generic T with Extensions" article on LinkedIn. You can read this article at: https://www.linkedin.com/pulse/using-enums-custom-attributes-jim-roton/

---
+++ GenericT_Sample/GenericT_Sample/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace GenericT_Sample
+{
+    public static class Extensions
+    {
+        public static T TryGet<T>(this Hashtable Table, string KeyName, T DefaultValue)
+        {
+            return (Table[KeyName].GetType() == typeof(T)) ?
+                (T)Table[KeyName] :
+                DefaultValue;
+        }
+    }
+}
---
+++ GenericT_Sample/GenericT_Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace GenericT_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable Table = new Hashtable();
+
+            Table.Add("IsConnected", true);
+            Table.Add("MyClass", new TestClass());
+
+            bool IsConnected = Table.TryGet<bool>("IsConnected", false);
+
+            TestClass MyClass = Table.TryGet<TestClass>("MyClass", null);
+        }
+
+        private class TestClass
+        {
+
+        }
+    }
+}
